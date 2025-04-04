379f1735b002751ea28cf9450ea591d99000b212
FabioMorrone <morronefabio04@gmail.com>
2025-04-04 18:00:50 +0200

    add MoviesProvider to App component and implement search input; update .gitignore for .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-
-import { CountProvider } from "./Context/MoviesContext"
+import { useState } from "react";
+import { MoviesProvider } from "./Context/MoviesContext"
 
 
 function App() {
 
+  const [movie, setMovie] = useState([])
+  const [searchMovie, setSearchMovie] = useState("")
+
+
 
   return (
-    <CountProvider>
+    <MoviesProvider>
+      <main>
 
-    </CountProvider>
+
+        <div class="mb-3">
+          <label for="" class="form-label"></label>
+          <input
+            type="text"
+            placeholder="Cerca il film..."
+            value={''}
+            onChange={''}
+          />
+          <button className="mt-3">Cerca</button>
+
+
+
+        </div>
+      </main>
+
+
+    </MoviesProvider>
   )
 }
 
