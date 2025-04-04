668a30cc68c6de53e169b4522c4e8780ae9ba7da
FabioMorrone <morronefabio04@gmail.com>
2025-04-04 18:25:49 +0200

    implement movie search functionality in App component; update input handling and display results

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,17 @@ import { MoviesProvider } from "./Context/MoviesContext"
 
 function App() {
 
+  const [tasks, setTasks] = useState("")
   const [movie, setMovie] = useState([])
-  const [searchMovie, setSearchMovie] = useState("")
+
+  const handleSearch = () => {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=123ca39c538b7ebadb458701979d7a72&query=${tasks}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovie(data.results || []);
+      })
+
+  };
 
 
 
@@ -14,15 +23,35 @@ function App() {
       <main>
 
 
-        <div class="mb-3">
-          <label for="" class="form-label"></label>
+        <div className="mb-3">
+          <label htmlFor="" className="form-label"></label>
           <input
             type="text"
             placeholder="Cerca il film..."
-            value={''}
-            onChange={''}
+            value={tasks}
+            onChange={(e) => setTasks(e.target.value)}
           />
-          <button className="mt-3">Cerca</button>
+          <button className="mt-3" onClick={handleSearch}>Cerca</button>
+          <div>
+            <ul>
+              {movie.map((film) => (
+                <li key={film.id}>
+                  <h3>{film.title}</h3>
+                  <p>Titolo Originale:{film.original_title}</p>
+                  <p>Lingua:{film.original_language}</p>
+                  <p>Voto:{film.vote_average}</p>
+                </li>
+
+
+
+
+
+
+
+
+              ))}
+            </ul>
+          </div>
 
 
 
