c45a34ba8f74280e65389efed019cd62cd5f82a1
FabioMorrone <morronefabio04@gmail.com>
2025-04-07 12:24:13 +0200

    add: implement TV series search functionality and display results

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,20 @@ function App() {
 
   const [tasks, setTasks] = useState("")
   const [movie, setMovie] = useState([])
+  const [serieTv, setSerieTv] = useState([])
+
 
 
   const handleSearch = () => {
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=123ca39c538b7ebadb458701979d7a72&query=${tasks}`)
+    fetch(`https://api.themoviedb.org/3/search/tv?api_key=123ca39c538b7ebadb458701979d7a72&language=it_IT&query=${tasks}`)
       .then(res => res.json())
       .then((data) => {
         setMovie(data.res || []);
+      })
+      .then(res => res.json())
+      .then((data) => {
+        setSerieTv(data.res || []);
       })
   };
 
@@ -40,6 +47,17 @@ function App() {
                   <p><strong>Lingua:</strong><img src={`https://flagcdn.com/24x18/${film.original_language}.png`}
                     alt={film.original_language} /></p>
                   <p><strong>Voto:{film.vote_average}</strong></p>
+                </li>
+              ))}
+            </ul>
+            <ul className="list-unstyled">
+              {serieTv.map((serie) => (
+                <li key={serie.id}>
+                  <h5><strong>Titolo:{serie.title}</strong></h5>
+                  <p><strong>Titolo Originale:{serie.original_title}</strong></p>
+                  <p><strong>Lingua:</strong><img src={`https://flagcdn.com/24x18/${serie.original_language}.png`}
+                    alt={serie.original_language} /></p>
+                  <p><strong>Voto:{serie.vote_average}</strong></p>
                 </li>
               ))}
             </ul>
